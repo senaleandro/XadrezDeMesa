7416a31eb176b5af26a94123077d9c402478b7f0
Leandro Sena <leandrosenax21@hotmail.com>
2024-08-01 12:11:44 -0300

    classe (Posicao) criada

---
+++ Tabuleiro/Posicao.cs
@@ -0,0 +1,25 @@
+
+
+using System.Runtime.InteropServices;
+
+namespace Tabuleiro
+{
+    public class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+          this.Linha = linha;
+          this.Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha
+            + ", "
+            + Coluna;
+        }
+    }
+}
